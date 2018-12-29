c1a99c8b220416e27b0adbd0459450b84c517742
joelthorner <joeltorner@gmail.com>
2018-12-29 20:56:03 +0100

    not found component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,37 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './views/home/home/home.component';
+import { NotFoundComponent } from './views/not-found/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  // {
+  //   path: 'about',
+  //   loadChildren: './views/about/about.module#AboutModule'
+  // },
+  // {
+  //   path: 'operations',
+  //   loadChildren: './views/operations/operations.module#OperationsModule'
+  // },
+  {
+    path: 'not-found',
+    component: NotFoundComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'not-found'
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, {
+      initialNavigation: 'enabled',
+    })
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
