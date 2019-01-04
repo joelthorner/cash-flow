78e0d0ccb190147717b6d11a9752d1a975413414
joelthorner <joeltorner@gmail.com>
2019-01-04 04:38:12 +0100

    operations

--- src/app/views/operations/new/new.component.ts
+++ src/app/views/operations/new/new.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Operation } from './../operation';
 
 @Component({
   selector: 'cf-new',
@@ -6,10 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new.component.scss']
 })
 export class NewComponent implements OnInit {
+  public kindsOfOperations = ['Income', 'Expense'];
+  public numberOfOperations = 0;
+  public operation: Operation = new Operation();
+  public operations: Operation[] = [];
+  public title = 'New Operation';
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() { }
+
+  public saveOperation() {
+    const clonedOperation = this.cloneOperation(this.operation);
+    this.operations.push(clonedOperation);
+    this.numberOfOperations = this.operations.length;
+    this.operation = new Operation();
+  }
+  public deleteOperation(operation: Operation) {
+    const index = this.operations.indexOf(operation);
+    this.operations.splice(index, 1);
+    this.numberOfOperations = this.operations.length;
   }
 
+  private cloneOperation(originalOperation: Operation): Operation {
+    const targetOperation = Object.assign({}, originalOperation);
+    return targetOperation;
+  }
 }
--- src/app/views/operations/operations.module.ts
+++ src/app/views/operations/operations.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { OperationsRoutingModule } from './operations-routing.module';
 import { OperationsComponent } from './operations/operations.component';
@@ -8,10 +9,16 @@ import { ListComponent } from './list/list.component';
 import { ItemComponent } from './item/item.component';
 
 @NgModule({
-  declarations: [OperationsComponent, NewComponent, ListComponent, ItemComponent],
+  declarations: [
+    OperationsComponent,
+    NewComponent,
+    ListComponent,
+    ItemComponent
+  ],
   imports: [
     CommonModule,
-    OperationsRoutingModule
+    OperationsRoutingModule,
+    FormsModule
   ]
 })
 export class OperationsModule { }
